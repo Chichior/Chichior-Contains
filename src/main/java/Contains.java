7f885e94ae76fb48258d18b7aa6aba9d3e72b029
Chichior <katiehakala@icloud.com>
2023-02-15 21:05:53 +0000

    ContainsComplete

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -11,6 +11,14 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
+        
+        for(int element : arr){
+        
+            if(element == target){
+                return true;
+            
+            }
+        }
         return false;
     }
 }
